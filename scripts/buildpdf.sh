8101ab8193d11afe68008f4cb27d2df3faeeace4
Sage Raflik <neonfuz@gmail.com>
2022-04-24 20:43:44 -0500

    Upgraded buildpdf script to find chromium-browser

--- scripts/buildpdf.sh
+++ scripts/buildpdf.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-CHROME=$(which chrome 2>/dev/null) || CHROME=$(which chromium)
+CHROME=$(which chrome 2>/dev/null) || CHROME=$(which chromium) || CHROME=$(which chromium-browser)
 
 mkdir -p docs
 echo '<!-- generated '`date +%s`' -->' > docs/index.html
-#pnpm run dev &
-#SERVER_PID=$!
-#sleep 3
+pnpm run dev &
+SERVER_PID=$!
+sleep 3
 $CHROME --headless --disable-gpu \
     --run-all-compositor-stages-before-draw \
     --font-render-hinting=medium \
     --print-to-pdf=docs/resume.pdf http://localhost:3000
-#kill $!
+kill $!
