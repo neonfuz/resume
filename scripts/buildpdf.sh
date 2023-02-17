8c5d72f39a4c62c44af4237fadcb342af7144e54
Sage Raflik <neonfuz@gmail.com>
2023-02-17 16:54:15 -0600

    Updates for sveltekit updates and macos support
    - Added path for google chrome on macos (default install location)
    - changed port from 3000 to 5173 to match new sveltekit default

--- scripts/buildpdf.sh
+++ scripts/buildpdf.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-
-
-
-CHROME=$(which chrome 2>/dev/null) || CHROME=$(which chromium) || CHROME=$(which chromium-browser)
+CHROME=$(which chrome 2>/dev/null) || CHROME=$(which chromium) || CHROME=$(which chromium-browser) || CHROME=/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
 
 mkdir -p docs
 echo '<!-- generated '`date +%s`' -->' > docs/index.html
@@ -11,8 +8,8 @@ echo '<meta http-equiv="Refresh" content="0; url='https://neonfuz.xyz'" />' >> d
 pnpm run dev &
 SERVER_PID=$!
 sleep 3
-$CHROME --headless --disable-gpu \
+"$CHROME" --headless --disable-gpu \
     --run-all-compositor-stages-before-draw \
     --font-render-hinting=none \
-    --print-to-pdf=docs/resume.pdf http://localhost:3000
+    --print-to-pdf=docs/resume.pdf http://localhost:5173
 kill $!
